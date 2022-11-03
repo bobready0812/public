daaad91186be8140ececde6d58eaa1cd05b712d3
DaehyeonKim <Daehyeonoff@gmail.com>
2022-11-03 22:28:56 +0900

    20221103

--- src/pages/Secret.jsx
+++ src/pages/Secret.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
+import {useCookies} from "react-cookie";
+import {ToastContainer, toast} from 'react-toastify';
+import axios from "axios";
+
 export default function Secret() {
   const navigate = useNavigate(); 
+  const [cookies, setCookie, removeCookie] = useCookies([]);
+  useEffect(() => {
+    const verifyUser = async () => {
+      if(!cookies.jwt) {
+        navigate("/login");
+
+       } else {
+          const {data} = await axios.post(
+            "http://localhost:4000/",{}, {
+              withCredentials:true
+            }
+          );
+          if(!data.status) {
+            removeCookie("jwt")
+            navigate("/login");
+          } else toast(`HI ${data.user}`, {theme:"dark"});
+        };
+      };
+      verifyUser();
+    
+  }, [cookies, navigate, removeCookie]);
   const logOut = () => {
+    removeCookie("jwt");
     navigate("/register"); 
+
   }
 
   return (
@@ -11,6 +38,7 @@ export default function Secret() {
     <div className='private'>
       <h1>Super Secret Page</h1>
       <button onClick={logOut}>Log Out</button>
+      <ToastContainer />
     </div>
   )
 }
