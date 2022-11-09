b4a80cb855a37d3b597f36470a686c4040de431f
DaehyeonKim <Daehyeonoff@gmail.com>
2022-11-09 22:26:54 +0900

    20221109

--- src/pages/Secret.jsx
+++ src/pages/Secret.jsx
@@ -32,7 +32,7 @@ export default function Secret() {
   }, [cookies, navigate, removeCookie]);
   const logOut = () => {
     removeCookie("jwt");
-    navigate("/register");
+    navigate("/login");
   }
 
   return (
