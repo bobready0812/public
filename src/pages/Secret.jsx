88ce03f55fa0cee71388bb370400907dde1713a3
DaehyeonKim <Daehyeonoff@gmail.com>
2022-11-30 22:19:43 +0900

    20221130

--- src/pages/Secret.jsx
+++ src/pages/Secret.jsx
@@ -43,7 +43,7 @@ export default function Secret() {
   return (
     <>
     <div className='private'>
-      <h1>Super Secret Pages</h1>
+      <h1>Secret Page</h1>
       <button onClick={logOut}>Log Out</button>
     </div>
     <ToastContainer />
