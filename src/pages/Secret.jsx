8f39512c358dd7a0431cc5c2f20cf46464106438
DaehyeonKim <Daehyeonoff@gmail.com>
2022-12-30 22:15:10 +0900

    20221230

--- src/pages/Secret.jsx
+++ src/pages/Secret.jsx
@@ -16,7 +16,7 @@ export default function Secret() {
        } else {
        
           const {data} = await axios.post(
-            "http://localhost:4000/",{}, {
+            "http://localhost:3030/",{}, {
              withCredentials:true,
             }
           );
